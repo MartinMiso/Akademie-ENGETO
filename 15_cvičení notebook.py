524a0f79d242cc1856c4bae0883391e2a7147416
MartinMiso <martin.miso@seznam.cz>
2024-12-01 22:06:42 +0100

    nový úkol

--- 15_cvičení notebook.py
+++ 15_cvičení notebook.py
@@ -48,3 +48,56 @@
 
 
 """DOMÁCÍ ÚKOL - HESLO"""
+# Vytvoř takový podmínkový zápis, který bude reagovat na nesprávně zadaná hesla (viz. příklad níže):
+# heslo_0 = ""            # FAIL -> "Vynechal jsi pole s heslem!"
+# heslo_1 = "1panpes738"  # FAIL -> "Heslo nesmí začínat číselným znakem"
+# heslo_2 = "panpessss"   # FAIL -> "Heslo musí obsahovat jak číselné znaky, tak písmena"
+# heslo_3 = "123456"      # FAIL -> "Heslo nesmí začínat číselným znakem"
+# heslo_4 = "aa1234"      # FAIL -> "Heslo musí být alespoň 8 znaků dlouhé"
+# heslo_5 = "p@npes7778"  # FAIL -> "Heslo nesmí obsahovat '@'"
+# heslo_6 = "panpes7778"  # PASS -> "Heslo je v pořádku"
+
+# while True:
+#     heslo = input("Zadej heslo: ")
+
+#     if heslo == "":
+#         print("Vynechal jsi pole s heslem!")
+#     elif heslo[0].isdigit():
+#         print("Heslo nesmí začínat číselným znakem")
+#     elif not (any(char.isdigit() for char in heslo) and any(char.isalpha() for char in heslo)):
+#         print("Heslo musí obsahovat jak číselné znaky, tak písmena")
+#     elif len(heslo) < 8:
+#         print("Heslo musí být alespoň 8 znaků dlouhé")
+#     elif "@" in heslo:
+#         print("Heslo nesmí obsahovat '@'")
+#     else:
+#         print("Heslo je v pořádku")
+#         break
+
+# -------------------------------
+# ŘEŠENÍ ENGETO
+
+# heslo_0 = ""            # FAIL -> "Vynechal jsi pole s heslem!"
+# heslo_1 = "1panpes738"  # FAIL -> "Heslo nesmí začínat číselným znakem"
+# heslo_2 = "panpessss"   # FAIL -> "Heslo musí obsahovat jak číselné znaky, tak písmena"
+# heslo_3 = "12345678"    # FAIL -> "Heslo nesmí začínat číselným znakem"
+# heslo_4 = "aa1234"      # FAIL -> "Heslo musí být alespoň 8 znaků dlouhé"
+# heslo_5 = "p@npes7778"  # FAIL -> "Heslo nesmí obsahovat '@'"
+# heslo_6 = "panpes7778"  # PASS -> "Heslo je v pořádku"
+
+# heslo = heslo_3
+
+# if not heslo:
+#     print("Vynechal jsi pole s heslem!")
+# elif heslo[0].isdigit() and not heslo.isnumeric():
+#     print("Heslo nesmí začínat číselným znakem")
+# elif len(heslo) < 8:
+#     print("Heslo musí být alespoň 8 znaků dlouhé")
+# elif heslo.find("@") != -1:
+#     print('Heslo nesmí obsahovat \"@\"')
+# elif heslo.isnumeric() or heslo.isalpha():
+#     print("Heslo musí obsahovat jak číselné znaky, tak písmena")
+# else:
+#     print("Heslo je v pořádku")
+
+
